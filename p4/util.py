6763fbd2cf6ddfe64d951089cb7c1cfbb0104308
josephburkhart <joseph.burkhart1@comcast.net>
2026-01-20 10:41:40 -0500

    Fix body orientations bug

--- p4/util.py
+++ p4/util.py
@@ -251,7 +251,7 @@ def add_rigid_constraint(
         for position in particle_model.secondary_positions_by_type[t]
     ]
 
-    if particle_model.secondary_orientations_by_type is not None:
+    if particle_model.secondary_orientations_by_type:
         orientations = [
             orientation
             for t in included_secondary_types

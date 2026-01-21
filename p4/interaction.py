4643c4a1dd2d2ab2452093449ff092b5e228ae7a
josephburkhart <joseph.burkhart1@comcast.net>
2026-01-21 17:51:27 -0500

    Add missing imports

--- p4/interaction.py
+++ p4/interaction.py
@@ -1,6 +1,8 @@
 # Copyright (c) 2025-2026, The Regents of the University of Michigan
 # This file is from the p4 project, released under the BSD 3-Clause License.
 
+from copy import deepcopy
+import inspect
 import itertools
 from typing import Iterable
 import hoomd

f1de8b79adc985893b23cabc40c909730034c16f
josephburkhart <joseph.burkhart1@comcast.net>
2026-01-22 16:05:11 -0500

    Create test_system.py

---
+++ test/test_system.py
@@ -0,0 +1,17 @@
+def test_instantiation_valid():
+    pass
+
+def test_instantiation_invalid():
+    pass
+
+def test_interactions():
+    pass
+
+def test_interacting_types():
+    pass
+
+def test_probe_potential():
+    pass
+
+def test_from_hoomd_simulation():
+    pass

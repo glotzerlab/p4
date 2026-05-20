23a278f996d7c94433c6745c8f7b537dce51e3b4
josephburkhart <joseph.burkhart1@comcast.net>
2026-05-20 12:52:49 -0400

    Rename variables for consistency

--- p4/body.py
+++ p4/body.py
@@ -1825,12 +1825,12 @@ class Body:
             )
             and
             all(
-                all(list(i) == [1,0,0,0] for i in other.orientations_by_type[t])
+                all(list(o) == [1,0,0,0] for o in other.orientations_by_type[t])
                 for t in orientations_missing_from_self
             )
             and
             all(
-                all(list(i) == [1,0,0,0] for i in self.orientations_by_type[t])
+                all(list(o) == [1,0,0,0] for o in self.orientations_by_type[t])
                 for t in orientations_missing_from_other
             )
         )

5aced9ec702a097eee889a9ac62776f6906fbaa4
josephburkhart <joseph.burkhart1@comcast.net>
2026-05-21 15:58:55 -0400

    Create basic measurement tests

---
+++ test/test_measure.py
@@ -0,0 +1,177 @@
+from pathlib import Path
+
+import pytest
+import hoomd
+import numpy as np
+import tempfile
+
+import p4
+
+REFERENCE_FOLDER = Path(__file__).parent / "data"
+
+def test_measure_same_probe_and_analyte():
+    """Ensure System.measure() does not error when the probe and analyte are identical."""
+    s = p4.System(
+        probe=p4.Body("A"),
+        analyte=p4.Body("A"),
+        interactions=[
+            p4.Interaction(
+                hoomd_class=hoomd.md.pair.LJ,
+                initial_args={},
+                default_params=dict(r_cut=5, params=dict(epsilon=1, sigma=1)),
+                typed_params={}
+            )
+        ]
+    )
+
+    with tempfile.TemporaryDirectory(dir=REFERENCE_FOLDER) as tempdir:
+        test_path = Path(tempdir) / f"test.csv"
+        s.measure(
+            quantities=["U", "F", "T"],
+            position_resolutions=[2, 2, 2],
+            orientation_resolutions=[2, 2, 2],
+            symmetries=[1, 1, 1],
+            nlist=hoomd.md.nlist.Tree(2),
+            csv_filename=test_path,
+            save_gsd=False,
+            outside_cutoff=20,
+        )
+
+def test_measure_single_particle_probe_multi_particle_body_analyte():
+    """Ensure System.measure() does not error when the probe is single-particle and the analyte is multi-particle body."""
+    s = p4.System(
+        probe=p4.Body("A"),
+        analyte=p4.Body("B", ["C"], dict(C=[[1, 0, 0]])),
+        interactions=[
+            p4.Interaction(
+                hoomd_class=hoomd.md.pair.LJ,
+                initial_args={},
+                default_params=dict(r_cut=0, params=dict(epsilon=1, sigma=1)),
+                typed_params={
+                    ("A", "B"): dict(r_cut=3, params=dict(epsilon=1, sigma=1)),
+                    ("A", "C"): dict(r_cut=3, params=dict(epsilon=1, sigma=2))
+                }
+            )
+        ]
+    )
+
+    with tempfile.TemporaryDirectory(dir=REFERENCE_FOLDER) as tempdir:
+        test_path = Path(tempdir) / f"test.csv"
+        s.measure(
+            quantities=["U", "F", "T"],
+            position_resolutions=[2, 2, 2],
+            orientation_resolutions=[2, 2, 2],
+            symmetries=[1, 1, 1],
+            nlist=hoomd.md.nlist.Tree(2),
+            csv_filename=test_path,
+            save_gsd=False,
+            outside_cutoff=20,
+        )
+
+def test_measure_multi_particle_probe_single_particle_body_analyte():
+    """Ensure System.measure() does not error when the probe is multi-particle and the analyte is a single-particle body."""
+    s = p4.System(
+        probe=p4.Body("B", ["C"], dict(C=[[1, 0, 0]])),
+        analyte=p4.Body("A"),
+        interactions=[
+            p4.Interaction(
+                hoomd_class=hoomd.md.pair.LJ,
+                initial_args={},
+                default_params=dict(r_cut=0, params=dict(epsilon=1, sigma=1)),
+                typed_params={
+                    ("A", "B"): dict(r_cut=3, params=dict(epsilon=1, sigma=1)),
+                    ("A", "C"): dict(r_cut=3, params=dict(epsilon=1, sigma=2))
+                }
+            )
+        ]
+    )
+
+    with tempfile.TemporaryDirectory(dir=REFERENCE_FOLDER) as tempdir:
+        test_path = Path(tempdir) / f"test.csv"
+        s.measure(
+            quantities=["U", "F", "T"],
+            position_resolutions=[2, 2, 2],
+            orientation_resolutions=[2, 2, 2],
+            symmetries=[1, 1, 1],
+            nlist=hoomd.md.nlist.Tree(2),
+            csv_filename=test_path,
+            save_gsd=False,
+            outside_cutoff=20,
+        )
+
+def test_measure_single_particle_probe_single_particle_arrangement_analyte():
+    """Ensure System.measure() does not error when the probe is multi-particle and the analyte is an arrangement of single-particle bodies."""
+    s = p4.System(
+        probe=p4.Body("A"),
+        analyte=p4.Arrangement([p4.Body("B"), p4.Body("C")], dict(B=[[0, 0, 0]], C=[[1, 0, 0]])),
+        interactions=[
+            p4.Interaction(
+                hoomd_class=hoomd.md.pair.LJ,
+                initial_args={},
+                default_params=dict(r_cut=0, params=dict(epsilon=1, sigma=1)),
+                typed_params={
+                    ("A", "B"): dict(r_cut=3, params=dict(epsilon=1, sigma=1)),
+                    ("A", "C"): dict(r_cut=3, params=dict(epsilon=1, sigma=2))
+                }
+            )
+        ]
+    )
+
+    with tempfile.TemporaryDirectory(dir=REFERENCE_FOLDER) as tempdir:
+        test_path = Path(tempdir) / f"test.csv"
+        s.measure(
+            quantities=["U", "F", "T"],
+            position_resolutions=[2, 2, 2],
+            orientation_resolutions=[2, 2, 2],
+            symmetries=[1, 1, 1],
+            nlist=hoomd.md.nlist.Tree(2),
+            csv_filename=test_path,
+            save_gsd=False,
+            outside_cutoff=20,
+        )
+
+def test_measure_single_particle_probe_multi_particle_arrangement_analyte():
+    """Ensure System.measure() does not error when the probe is multi-particle and the analyte is an arrangement of multi-particle bodies."""
+    s = p4.System(
+        probe=p4.Body("A"),
+        analyte=p4.Arrangement(
+            bodies=[
+                p4.Body(
+                    primary_type="B",
+                    secondary_types=["D"],
+                    positions_by_type=dict(D=[[1, 0, 0]])
+                ),
+                p4.Body(
+                    primary_type="C",
+                    secondary_types=["E"],
+                    positions_by_type=dict(E=[[-1, 0, 0]])
+                )
+            ],
+            positions_by_type=dict(B=[[0, 0, 0]], C=[[1, 0, 0]]),
+            orientations_by_type=dict(B=[[1, 0, 0, 0]], C=[[0, 0.707, 0.707, 0]])
+        ),
+        interactions=[
+            p4.Interaction(
+                hoomd_class=hoomd.md.pair.LJ,
+                initial_args={},
+                default_params=dict(r_cut=0, params=dict(epsilon=1, sigma=1)),
+                typed_params={
+                    ("A", "D"): dict(r_cut=3, params=dict(epsilon=1, sigma=1)),
+                    ("A", "E"): dict(r_cut=3, params=dict(epsilon=1, sigma=2))
+                }
+            )
+        ]
+    )
+
+    with tempfile.TemporaryDirectory(dir=REFERENCE_FOLDER) as tempdir:
+        test_path = Path(tempdir) / f"test.csv"
+        s.measure(
+            quantities=["U", "F", "T"],
+            position_resolutions=[2, 2, 2],
+            orientation_resolutions=[2, 2, 2],
+            symmetries=[1, 1, 1],
+            nlist=hoomd.md.nlist.Tree(2),
+            csv_filename=test_path,
+            save_gsd=False,
+            outside_cutoff=20,
+        )

ef2a2aa62f2043bafa93e641881c283a74c4e6a9
josephburkhart <joseph.burkhart1@comcast.net>
2026-06-02 16:22:28 -0400

    Add copyright to tests

--- test/test_field.py
+++ test/test_field.py
@@ -1,3 +1,6 @@
+# Copyright (c) 2025-2026, The Regents of the University of Michigan
+# This file is from the p4 project, released under the BSD 3-Clause License.
+
 from pathlib import Path
 
 import pytest
--- test/test_plotting.py
+++ test/test_plotting.py
@@ -1,3 +1,6 @@
+# Copyright (c) 2025-2026, The Regents of the University of Michigan
+# This file is from the p4 project, released under the BSD 3-Clause License.
+
 import coxeter
 import pytest
 from copy import copy

c613ab4c9eafb2d0e3636f7e2c5a3cdb19688010
josephburkhart <joseph.burkhart1@comcast.net>
2026-06-20 14:19:38 -0400

    Fix bug in simulation box calculation

--- p4/system.py
+++ p4/system.py
@@ -704,9 +704,9 @@ class System:
                 max_r_cut = max(max_r_cut, param_dict.get("r_cut", 0))
         
         simulation_box = 1.1 * np.array([
-            2 * (max_distance + max_r_cut),
-            2 * (max_distance + max_r_cut),
-            2 * (max_distance + max_r_cut),
+            2 * (max_distance + max_r_cut + max_distance),
+            2 * (max_distance + max_r_cut + max_distance),
+            2 * (max_distance + max_r_cut + max_distance),
             0.0,
             0.0,
             0.0

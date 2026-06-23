Fix issue in measurement simulation box calculation

p4/system.py
@@ -649,9 +649,9 @@ class System:
                 max_r_cut = max(max_r_cut, param_dict.get("r_cut", 0))
         
         simulation_box = 1.1 * np.array([
-            2 * (max_distance + max_r_cut + max_distance),
-            2 * (max_distance + max_r_cut + max_distance),
-            2 * (max_distance + max_r_cut + max_distance),
+            1.1 * 2 * ((max_distance + max_r_cut) + nlist.buffer),
+            1.1 * 2 * ((max_distance + max_r_cut) + nlist.buffer),
+            1.1 * 2 * ((max_distance + max_r_cut) + nlist.buffer),
             0.0,
             0.0,
             0.0